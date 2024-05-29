d6463d062a424c61e60cd75402a8b798c97e9cc5
moustaxx <maciasm1303@gmail.com>
2024-05-29 16:44:05 +0200

    Edit lab4

--- lab4/neuron.py
+++ lab4/neuron.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 class Neuron:
     def __init__(self, n_inputs, bias = 0., weights = None):
@@ -26,30 +27,30 @@ class ANN:
         return x
 
 def visualize_ann(layer_sizes):
-    fig, ax = plt.subplots(figsize=(12, 8))
+    fig, ax = plt.subplots(figsize=(10, 6))
     ax.axis('off')
 
     colors = ['red', 'blue', 'blue', 'green']
 
     layer_xs = np.linspace(0, 1, len(layer_sizes))
     for i, (n_neurons, x, color) in enumerate(zip(layer_sizes, layer_xs, colors)):
-        y_space = np.linspace(0, 1, n_neurons)
+        y_space = np.linspace(0, 1, n_neurons + 2)[1:-1]
 
-        if y_space.argmax() == 0:
-            y_space = np.array([0.5])
         for y in y_space:
-            ax.plot(x, y, 'o', markersize=15, color=color)
+            ax.plot(x, y, 'o', markersize=30, color=color)
 
         if i > 0:
-            prev_y_space = np.linspace(0, 1, layer_sizes[i-1])
+            prev_y_space = np.linspace(0, 1, layer_sizes[i-1] + 2)[1:-1]
             for y in y_space:
                 for prev_y in prev_y_space:
-                    ax.plot([layer_xs[i-1], x], [prev_y, y], 'k-')
+                    dx = x-layer_xs[i-1]
+                    dy = y - prev_y
+                    ax.add_patch(patches.Arrow(layer_xs[i-1], prev_y, dx, dy, width=0.015, color='k'))
 
-    ax.text(layer_xs[0] - 0.05, -0.1, 'input layer', va='center', ha='center', color='red', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
-    ax.text(layer_xs[1] - 0.05, -0.1, 'hidden layer 1', va='center', ha='center', color='blue', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
-    ax.text(layer_xs[2] - 0.05, -0.1, 'hidden layer 2', va='center', ha='center', color='blue', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
-    ax.text(layer_xs[3] - 0.05, -0.1, 'output layer', va='center', ha='center', color='green', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
+    ax.text(layer_xs[0] - 0.05, 0.11, 'input layer', va='center', ha='center', color='red', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
+    ax.text(layer_xs[1] - 0.05, 0.11, 'hidden layer 1', va='center', ha='center', color='blue', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
+    ax.text(layer_xs[2] - 0.05, 0.11, 'hidden layer 2', va='center', ha='center', color='blue', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
+    ax.text(layer_xs[3] - 0.05, 0.11, 'output layer', va='center', ha='center', color='green', fontsize=14, bbox=dict(facecolor='white', alpha=0.6))
 
     plt.show()
 
